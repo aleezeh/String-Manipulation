680334740dc5ab078abcf54b8ed8b55117eeb5c2
Aleezeh Zehra Shehzad <55654062+aleezeh@users.noreply.github.com>
2019-10-09 22:19:14 +0500

    1) Name Picker
    Picks first name of String one and second name of String 2.

---
+++ Name Picker(GitHub) .vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Str2, myString, firstName, lastName As String
+        Dim spacePos1, spacePos2 As Integer
+
+        Str1 = ""
+        Str2 = ""
+        myString = ""
+        firstName = ""
+        lastName = ""
+        spacePos1 = 0
+        spacePos2 = 0
+
+        Console.Write("Enter first string: ")
+        Str1 = Console.ReadLine
+
+        Console.Write("Enter second string: ")
+        Str2 = Console.ReadLine
+
+        spacePos1 = InStr(Str1, " ")
+        firstName = Mid(Str1, 1, spacePos1 - 1)
+
+        spacePos2 = InStr(Str2, " ")
+        lastName = Mid(Str2, spacePos2 + 1, Len(Str2))
+
+        myString = firstName & " " & lastName
+
+        Console.WriteLine(myString)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
