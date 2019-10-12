53bc8951f10f3fd043a023f77bdf967857a0ef35
Aleezeh Zehra Shehzad <55654062+aleezeh@users.noreply.github.com>
2019-10-12 21:46:45 +0500

    Shows isolated words of a string.

---
+++ Word Separator (GitHub).vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Str2, nextChar As String
+        Dim Count As Integer
+
+        Str1 = ""
+        Str2 = ""
+        nextChar = ""
+        Count = 0
+
+        Console.Write("Enter string: ")
+        Str1 = Console.ReadLine
+
+        For Count = 1 To Len(Str1)
+            nextChar = Mid(Str1, Count, 1)
+            If nextChar <> " " Then
+                Str2 = Str2 & nextChar
+            Else
+                Console.WriteLine(Str2)
+                Str2 = ""
+            End If
+        Next
+
+        Console.WriteLine(Str2)
+        Console.ReadKey()
+    End Sub
+
+End Module
