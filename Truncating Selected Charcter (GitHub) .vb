c0da167410b7fbd967063bbd50249e4196052bcb
Aleezeh Zehra Shehzad <55654062+aleezeh@users.noreply.github.com>
2019-10-12 22:00:18 +0500

    Removes the selected character from a string.

---
+++ Truncating Selected Charcter (GitHub) .vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, newString, nextChar, remChar As String
+        Dim Count As Integer
+
+        Str1 = ""
+        newString = ""
+        nextChar = ""
+        remChar = ""
+        Count = 0
+
+        Console.Write("Enter string: ")
+        Str1 = Console.ReadLine
+
+        Console.Write("Enter the character that has to be removed: ")
+        remChar = Console.ReadLine
+
+        For Count = 1 To Len(Str1)
+            nextChar = Mid(Str1, Count, 1)
+            If nextChar <> remChar Then
+                newString = newString & nextChar
+            End If
+        Next
+
+        Console.WriteLine(newString)
+        Console.ReadKey()
+    End Sub
+
+End Module
