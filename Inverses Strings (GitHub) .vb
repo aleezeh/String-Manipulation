e40dd7ebfdb3fc696833143b5960088789e6d6ee
Aleezeh Zehra Shehzad <55654062+aleezeh@users.noreply.github.com>
2019-10-12 22:12:14 +0500

    Inverses the string input.

---
+++ Inverses Strings (GitHub) .vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, newString, nextChar As String
+        Dim Count As Integer
+
+        Str1 = ""
+        newString = ""
+        nextChar = ""
+        Count = 0
+
+        Console.Write("Enter string to inverse: ")
+        Str1 = Console.ReadLine
+
+        For Count = Len(Str1) To 1 Step -1
+            nextChar = Mid(Str1, Count, 1)
+            newString = newString & nextChar
+        Next
+
+        Console.WriteLine(newString)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
